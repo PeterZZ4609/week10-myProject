d5e00954324b742e2156c1b38900920e2e526ea9
PeterZhang <13912151813@163.com>
2020-11-13 13:55:34 +0800

    module_course OK

--- module_course/course.c
+++ module_course/course.c
@@ -3,11 +3,34 @@
 
 #include "course.h"
 
-int delete_course(int id);
+int delete_course(int id)
+{
+    return course_delete(id);
+}
 
-int add_course(course_info *info);
+int add_course(course_info *info)
+{
+    entry_course c;
+    c.id = info->id;
+    c.name = info->name;
+    c.type = info->type;
+    c.hours = info->hours;
+    c.credit = info->credit;
+    c.students_limit = info->students_limit;
+    return course_insert(&c);
+}
 
-int edit_course(int id, course_info *info);
+int edit_course(int id, course_info *info)
+{
+    entry_course c;
+    c.id = info->id;
+    c.name = info->name;
+    c.type = info->type;
+    c.hours = info->hours;
+    c.credit = info->credit;
+    c.students_limit = info->students_limit;
+    return course_edit(id, &c);
+}
 
 course_info *get_all_courses_info()
 {
@@ -28,21 +51,30 @@ course_info *get_all_courses_info()
             this->credit = entries->credit;
             this->students_limit = entries->students_limit;
 
+            this->students = NULL;
+
             // students
-            entry_student *students = (entry_student *)malloc(sizeof(entry_student));
             entry_selection *selections = selection_find_course(this->id);
             if (selections)
             {
-                // entry_student *this = 
-            }
-            else
-            {
-                this->students = NULL;
+                entry_student *students = (entry_student *)malloc(sizeof(entry_student));
+                students->next = NULL;
+                entry_student *now = students;
+                while (selections)
+                {
+                    entry_student *this = student_select_by_id(selections->stu_id);
+                    now->next = this;
+                    now = now->next;
+                    selections = selections->next;
+                }
+                this->students = students->next; // need free()
             }
             now->next = this;
             now = now->next;
             entries = entries->next;
         }
+
+        return courses->next;
     }
     else
     {
