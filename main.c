aee2932ec9e5cb168e061d8cb007b3504fde5c1e
PeterZhang <13912151813@163.com>
2020-11-15 22:46:24 +0800

    module_main::student_management OK

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "student.h"
 #include "course.h"
@@ -22,7 +23,7 @@ int main(void)
     while (1)
     {
         // 主菜单
-        puts("----------------");
+        puts(SPLINE_1);
         puts("1. 管理学生信息");
         puts("2. 管理课程信息");
         puts("0. 退出");
@@ -48,7 +49,9 @@ int main(void)
 char wait_option()
 {
     printf("> ");
-    char c = getchar();
+    char c;
+    while ('\n' == (c = getchar()))
+        ;
     while ('\n' != getchar())
         ;
     fflush(stdin);
@@ -57,31 +60,114 @@ char wait_option()
 
 void manage_student()
 {
-    puts(SPLINE_1);
-    // puts("1");
-    student_info *all = get_all_students_info();
-    student_info *iter = all;
-    // puts("1.end");
-    while (iter)
+    char flag = 1;
+    while (flag)
     {
-        // puts("2");
-        printf("%s ID:%d\n", iter->name, iter->id);
-        entry_course *iter2 = iter->courses;
-        if (!iter2)
-        {
-            printf("%s空\n", INDENT);
-        }
-        else
-        {
-            // puts("3");
-            while (iter2)
-            {
-                printf("%s(%d)%s %s %d学时 %.1f学分\n", INDENT, iter2->id, iter2->name, iter2->type, iter2->hours, iter2->credit);
-                iter2 = iter2->next;
-            }
-        }
+
         puts(SPLINE_2);
-        iter = iter->next;
+        puts("1. 添加学生信息");
+        puts("2. 删除学生信息");
+        puts("3. 修改学生信息");
+        puts("4. 查看所有学生信息");
+        puts("0. 返回上一级");
+        char op = wait_option();
+        switch (op)
+        {
+        case '1':
+            puts(SPLINE_2);
+            puts("学生ID：");
+            int id;
+            scanf("%d", &id);
+            puts("学生姓名：");
+            char name[1024];
+            scanf("%s", name);
+            puts("添加中...");
+            if (student_insert(id, name) == 1)
+                puts("添加成功！");
+            else
+                puts("添加失败！");
+            break;
+        case '2':
+            puts(SPLINE_2);
+            puts("请输入将要删除的学生ID：");
+            int id2;
+            scanf("%d", &id2);
+            entry_student *stu2 = student_select_by_id(id2);
+            if (stu2 == NULL)
+            {
+                puts("该学生不存在！");
+            }
+            else
+            {
+                puts("删除中...");
+                student_delete(id2);
+                puts("删除成功！");
+            }
+            break;
+        case '3':
+            puts("请输入将要修改的学生的ID：");
+            int id3;
+            scanf("%d", &id3);
+            entry_student *stu3 = student_select_by_id(id3);
+            if (stu3 == NULL)
+            {
+                puts("该学生不存在！");
+            }
+            else
+            {
+                puts("输入新ID（0表示不修改）：");
+                int new_id;
+                scanf("%d", &new_id);
+                if (new_id > 0)
+                {
+                    stu3->id = new_id;
+                }
+                puts("输入新的姓名：");
+                char name[1024];
+                scanf("%s", name);
+                if (strlen(name) > 0)
+                {
+                    stu3->name = name;
+                }
+                puts("修改中：");
+                if (student_edit(id3, stu3))
+                    puts("\033[01;32m修改成功！\033[0m");
+                else
+                    puts("修改失败！");
+            }
+            break;
+        case '4':
+            puts(SPLINE_2);
+            student_info *all = get_all_students_info();
+            student_info *iter = all;
+            while (iter)
+            {
+                // puts("2");
+                printf("%s ID:%d\n", iter->name, iter->id);
+                entry_course *iter2 = iter->courses;
+                if (!iter2)
+                {
+                    printf("%s空\n", INDENT);
+                }
+                else
+                {
+                    // puts("3");
+                    while (iter2)
+                    {
+                        printf("%s(%d)%s %s %d学时 %.1f学分\n", INDENT, iter2->id, iter2->name, iter2->type, iter2->hours, iter2->credit);
+                        iter2 = iter2->next;
+                    }
+                }
+                puts(SPLINE_2);
+                iter = iter->next;
+            }
+            break;
+        case '0':
+            flag = 0;
+            break;
+        default:
+            break;
+        }
     }
 }
 
